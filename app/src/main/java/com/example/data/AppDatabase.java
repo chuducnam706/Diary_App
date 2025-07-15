547739410ac88fca144dbbae25d8d930a3aac00b
20222595 <20222595@eaut.edu.vn>
2025-07-16 00:19:10 +0700

    đây là mã nguồn đầu tiên

--- app/src/main/java/com/example/data/AppDatabase.java
+++ app/src/main/java/com/example/data/AppDatabase.java
@@ -4,6 +4,8 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
+import com.example.ConverLocaleDateTime;
+
 @Database(entities = {DiaryEntity.class}, version = 1)
 @TypeConverters({ConverLocaleDateTime.class})
 public abstract class AppDatabase extends RoomDatabase {
